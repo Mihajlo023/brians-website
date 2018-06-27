255e64902d465b95cc36ab57148dcf4d02c92b8d
Thomas <bwt@amazon.com>
2018-06-26 21:06:49 -0700

    Changes made for congito user creation

---
+++ js/init.js
@@ -0,0 +1,13 @@
+// CONSTANTS
+var COGNITO_USER_POOL_ID = "us-west-2_yZ4ajmJnS";
+Object.freeze(COGNITO_USER_POOL_ID);
+
+var COGNITO_CLIENT_ID = "5op4pu0pl4vjkumgjvmavrdtan";
+Object.freeze(COGNITO_CLIENT_ID);
+
+var COGNITO_POOL_DATA = {
+    UserPoolId : COGNITO_USER_POOL_ID,
+    ClientId : COGNITO_CLIENT_ID
+};
+Object.freeze(COGNITO_POOL_DATA);
+
---
+++ js/login.js
@@ -0,0 +1,59 @@
+// functions
+
+var userPool = new AmazonCognitoIdentity.CognitoUserPool(COGNITO_POOL_DATA);
+
+function createUser() {
+    let attributeList = [];
+
+    let dataFirstName = {
+        Name : "name",
+        Value : $("#first_name").val()
+    };
+
+    let dataLastName = {
+        Name : "family_name",
+        Value : $("#last_name").val()
+    };
+
+    let dataEmail = {
+        Name :"email",
+        Value : $("#email").val()
+    };
+
+    //TODO OPTIONAL TO DO LATER
+    
+    let attributeFirstName = new AmazonCognitoIdentity.CognitoUserAttribute(dataFirstName);
+    let attributeLastName = new AmazonCognitoIdentity.CognitoUserAttribute(dataLastName);
+    let attributeEmail = new AmazonCognitoIdentity.CognitoUserAttribute(dataEmail);
+
+    attributeList.push(attributeFirstName);
+    attributeList.push(attributeLastName);
+    attributeList.push(attributeEmail);
+
+    userPool.signUp(dataEmail.Value, $("#password").val(), attributeList, null, function(err, result) {
+        if (err) {
+            alert(err);
+            return;
+        }
+        cognitoUser = result.user;
+    console.log('user name is ' + cognitoUser.getUsername());
+    });
+};
+
+
+
+// JQUERY
+$("#register").click(function() {
+    createUser();
+});
+
+/*
+userPool.signUp('brianwthomas@gmail.com', '!Passw0rd', attributeList, null, function(err, result){
+    if (err) {
+        alert(err);
+        return;
+    }
+    cognitoUser = result.user;
+    console.log('user name is ' + cognitoUser.getUsername());
+});
+*/
