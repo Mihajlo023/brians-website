df4689c5bbcdf4f1515f32715bb93f5f943d3ab0
Radmilovic <mihajlo023@gmail.com>
2018-07-10 06:30:05 +0200

    desktop form done

--- js/settings.js
+++ js/settings.js
@@ -1,13 +1,21 @@
 
-function edit() {
-    let editInput = event.target.parentNode.children[0];
-    event.target.classList.add("d-none");
-    editInput.classList.remove("d-none");
-    editInput.classList.add("d-flex");
-}
-
-
 $(function() {    
+    const editPencilIconHtml = '<i class="fas fa-pencil-alt d-none"></i>';
+
+    $(".profile-item").click(function(event) {
+        if(event.target.nodeName != "BUTTON") {
+            const plainProfileText = $(this).find(".col-md-12");
+            plainProfileText.addClass("d-none");   
+             $(this).find("div.w-100").removeClass("d-none").addClass("d-flex");
+        }
+    });
+
+    $("button.close").click(function(event) {
+        const parent = $(this).parents(".form-row");
+        parent.children().first().removeClass("d-flex").addClass("d-none");
+        parent.children().eq(1).removeClass("d-none");
+    });
+
     $(".profile-name-save").click(function(event){
         event.preventDefault();
         const parent = $(this).parents(".form-row");
@@ -15,17 +23,42 @@ $(function() {
         const lastName = $("#inputLastName").val();
         const displayedName = $(".profile-name");
 
-        displayedName.html(firstName + " " + lastName);      
+        displayedName.html(firstName + " " + lastName).append(editPencilIconHtml);     
         parent.children().first().removeClass("d-flex").addClass("d-none");
         parent.children().eq(1).removeClass("d-none");
-        $(".desktop-form div.border-bottom:hover i").css("display: initial;");
     });
 
     $(".profile-bday-save").click(function(event) {
         event.preventDefault();
         const parent = $(this).parents(".form-row");
         const birthday = $("#inputBirthday").val();
-        console.log(birthday);
+        const displayedBirthday = $(".profile-bday");
+        displayedBirthday.html(birthday).append(editPencilIconHtml);
+        parent.children().first().removeClass("d-flex").addClass("d-none");
+        parent.children().eq(1).removeClass("d-none");
+    });
+
+    $(".profile-address-save").click(function(event) {
+        event.preventDefault();
+        const parent = $(this).parents(".form-row");
+        const city = $("#inputCity").val();
+        const postalCode = $("#inputPostalCode").val();
+        const displayedAddress = $(".profile-address");
+
+        displayedAddress.html(city + ", " + postalCode).append(editPencilIconHtml);
+        parent.children().first().removeClass("d-flex").addClass("d-none");
+        parent.children().eq(1).removeClass("d-none");
+    });
+
+    $(".profile-phone-save").click(function(event) {
+        event.preventDefault();
+        const parent = $(this).parents(".form-row");
+        const phone = $("#inputPhone").val();
+        const displayedPhone = $(".profile-phone");
+
+        displayedPhone.html(phone).append(editPencilIconHtml);;
+        parent.children().first().removeClass("d-flex").addClass("d-none");
+        parent.children().eq(1).removeClass("d-none");
     });
 });
 
